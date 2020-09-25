c41b86dd2d87eb57f43254b06344ab973c2a5050
Mana <manakouwenberg@gmail.com>
2020-09-25 09:58:20 +0200

    Added comments

--- SimonSays/SimonSays/SimonSays.cpp
+++ SimonSays/SimonSays/SimonSays.cpp
@@ -19,6 +19,7 @@ int main()
 	{
 		int i = rand() % 10;
 
+		//Create a number of the specified size
 		for (size_t j = 0; j < startAmount-1; j++)
 		{
 			i = appendRandom(i);
@@ -27,6 +28,10 @@ int main()
 	}
 }
 
+/// <summary>
+/// Play out a game of simon says with integer i. Recurses if the player is correct, returns if the player loses.
+/// </summary>
+/// <param name="i"></param>
 void simonSays(int i)
 {
 	system("CLS");
@@ -34,6 +39,7 @@ void simonSays(int i)
 	std::cout << i << std::endl;
 	sleep_for(milliseconds(1000));
 	system("CLS");
+
 	int r;
 	std::cin >> r;
 	
@@ -48,6 +54,11 @@ void simonSays(int i)
 	sleep_for(milliseconds(1000));
 }
 
+/// <summary>
+/// Appends a random number between 0 and 9 to an int
+/// </summary>
+/// <param name="i"></param>
+/// <returns></returns>
 int appendRandom(int i)
 {
 	i *= 10;
