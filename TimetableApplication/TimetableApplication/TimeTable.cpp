e9e33728eaf94f6b7eee363db6a7442e2a8680d3
Mana <manakouwenberg@gmail.com>
2020-09-25 11:18:29 +0200

    Started timetable assignment (Current code errors for unknown reasons)

---
+++ TimetableApplication/TimetableApplication/TimeTable.cpp
@@ -0,0 +1,13 @@
+#include "TimeTable.h"
+#include "Student.h"
+#include "Teacher.h"
+
+TimeTable::TimeTable(Student student)
+{
+	
+}
+
+TimeTable::TimeTable(Teacher teacher)
+{
+
+}
---
+++ TimetableApplication/TimetableApplication/TimeTable.h
@@ -0,0 +1,8 @@
+#pragma once
+class TimeTable
+{
+public:
+    TimeTable(Teacher teacher);
+    TimeTable(Student student);
+};
+
