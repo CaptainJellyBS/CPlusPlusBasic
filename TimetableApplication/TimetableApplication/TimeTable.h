a3b117d425247bc3a0940b0e6571db9d91beb68b
Mana <manakouwenberg@gmail.com>
2020-09-28 12:08:21 +0200

    Finished assignment, I think?

--- TimetableApplication/TimetableApplication/TimeTable.h
+++ TimetableApplication/TimetableApplication/TimeTable.h
@@ -2,6 +2,9 @@
 #include "Teacher.h"
 #include "Student.h"
 #include "Classroom.h"
+#include "Lesson.h"
+#include <map>
+#include <list>
 
 class TimeTable
 {
@@ -9,5 +12,11 @@ public:
     TimeTable(Teacher teacher);
     TimeTable(Student student);
     TimeTable(Classroom classroom);
+
+    //Dictionary holding all lessons, with an int representing the day as key, and a list of lessons for each day.
+    std::map<int, std::list<Lesson>> lessons;
+
+private:
+    void addCourseLessonsToDictionary(Course course);
 };
 
